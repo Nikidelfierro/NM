4d023cca71c94cf56b1d6d0487c68d0200b7ff1c
Nikidelfierro <landrieltristan1@gmail.com>
2023-01-14 12:41:24 -0300

    login con error

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const express = require ("express");
 const app = express();
 const bodyParser = require ("body-parser");
+const session = require ("express-session");
+const cookieParser = require ("cookie-parser");
 
 app.use (express.static(path.join(__dirname, "../public")));
 app.use (bodyParser.urlencoded({ extended: true}));
@@ -10,15 +12,27 @@ app.use (bodyParser.urlencoded({ extended: true}));
 
 app.set("view engine","ejs");
 
+const oneDay = 1000* 60* 60* 24;
+const halfDay = oneDay / 2;
+
+app.use(session({
+    saveUninitialized:true,
+    secret: process.env.SESSION_SECRET,
+    cookie: {maxAge: oneDay},
+    resave: false,
+}));
+
 
 const homeRoutes = require ("./routes/home");
 const productRoutes = require ("./routes/products");
 const userRoutes = require ("./routes/users");
+const loginRoutes = require ("./routes/signin");
 
 app.use("/",userRoutes);
 app.use("/", homeRoutes);
 app.use("/products", productRoutes);
-app.use("/users", userRoutes );
+app.use("/users", userRoutes);
+app.use("/signin",loginRoutes);
 
 
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -6,41 +6,60 @@ const router = express.Router();
 const products = [
   {
     id:1,
+    price:999,
     name: "producto 1",
     description: "first product",
     iamge: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fes.chevrolet.com%2Fcontent%2Fdam%2Fchevrolet%2Fna%2Fus%2Fenglish%2Findex%2Fvehicles%2F2023%2Fperformance%2Fcamaro%2F01-images%2Fcolorizer%2Fcoupe%2F2023-camaro-2ss-g7c-colorizer.jpg%3Fimwidth%3D960&imgrefurl=https%3A%2F%2Fes.chevrolet.com%2Fperformance%2Fcamaro&tbnid=aUpk1W21pdbE_M&vet=12ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ..i&docid=7jP3J2A_it7y9M&w=1024&h=512&q=camaro&client=opera&ved=2ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ"
   },
   {
     id:2,
+    price:999,
     name: "producto 2",
     description: "second product",
     iamge: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fes.chevrolet.com%2Fcontent%2Fdam%2Fchevrolet%2Fna%2Fus%2Fenglish%2Findex%2Fvehicles%2F2023%2Fperformance%2Fcamaro%2F01-images%2Fcolorizer%2Fcoupe%2F2023-camaro-2ss-g7c-colorizer.jpg%3Fimwidth%3D960&imgrefurl=https%3A%2F%2Fes.chevrolet.com%2Fperformance%2Fcamaro&tbnid=aUpk1W21pdbE_M&vet=12ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ..i&docid=7jP3J2A_it7y9M&w=1024&h=512&q=camaro&client=opera&ved=2ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ"
   },
   {
     id:3,
+    price:999,
     name: "producto 3",
     description: "third product",
     iamge: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fes.chevrolet.com%2Fcontent%2Fdam%2Fchevrolet%2Fna%2Fus%2Fenglish%2Findex%2Fvehicles%2F2023%2Fperformance%2Fcamaro%2F01-images%2Fcolorizer%2Fcoupe%2F2023-camaro-2ss-g7c-colorizer.jpg%3Fimwidth%3D960&imgrefurl=https%3A%2F%2Fes.chevrolet.com%2Fperformance%2Fcamaro&tbnid=aUpk1W21pdbE_M&vet=12ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ..i&docid=7jP3J2A_it7y9M&w=1024&h=512&q=camaro&client=opera&ved=2ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ"
   },
   {
     id:4,
+    price:999,
     name: "producto 4",
     description: "fourth product",
+    iamge: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fes.chevrolet.com%2Fcontent%2Fdam%2Fchevrolet%2Fna%2Fus%2Fenglish%2Findex%2Fvehicles%2F2023%2Fperformance%2Fcamaro%2F01-images%2Fcolorizer%2Fcoupe%2F2023-camaro-2ss-g7c-colorizer.jpg%3Fimwidth%3D960&imgrefurl=https%3A%2F%2Fes.chevrolet.com%2Fperformance%2Fcamaro&tbnid=aUpk1W21pdbE_M&vet=12ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ..i&docid=7jP3J2A_it7y9M&w=1024&h=512&q=camaro&client=opera&ved=2ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ"
+  } ,
+   {
+    id:5,
+    price:999,
+    name: "producto 5",
+    description: "fifth product",
+    iamge: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fes.chevrolet.com%2Fcontent%2Fdam%2Fchevrolet%2Fna%2Fus%2Fenglish%2Findex%2Fvehicles%2F2023%2Fperformance%2Fcamaro%2F01-images%2Fcolorizer%2Fcoupe%2F2023-camaro-2ss-g7c-colorizer.jpg%3Fimwidth%3D960&imgrefurl=https%3A%2F%2Fes.chevrolet.com%2Fperformance%2Fcamaro&tbnid=aUpk1W21pdbE_M&vet=12ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ..i&docid=7jP3J2A_it7y9M&w=1024&h=512&q=camaro&client=opera&ved=2ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ"
+  },
+  {
+    id:6,
+    price:999,
+    name: "producto 6",
+    description: "sixth product",
     iamge: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fes.chevrolet.com%2Fcontent%2Fdam%2Fchevrolet%2Fna%2Fus%2Fenglish%2Findex%2Fvehicles%2F2023%2Fperformance%2Fcamaro%2F01-images%2Fcolorizer%2Fcoupe%2F2023-camaro-2ss-g7c-colorizer.jpg%3Fimwidth%3D960&imgrefurl=https%3A%2F%2Fes.chevrolet.com%2Fperformance%2Fcamaro&tbnid=aUpk1W21pdbE_M&vet=12ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ..i&docid=7jP3J2A_it7y9M&w=1024&h=512&q=camaro&client=opera&ved=2ahUKEwjRz4_pnq78AhWatZUCHZAHDG8QMygBegUIARDkAQ"
   }
 ];
 
 router.get("/", (req, res) => {
-  const {id} = req.params;
-  return res.status(200).render("vistas/list.ejs",{ products,});
+  return res.status(200).render("vistas/home.ejs", {
+    products,
+  });
 });
 
-router.get("/", (req, res) => {
+router.get("/:id", (req, res) => {
   const {id} = req.params;
 
   const product = products.find((product) => product.id == id);
 
-  return res.status(200).render("vistas/details.ejs",{ product, });
+  return res.status(200).render("vistas/serviceDetail.ejs",{ product, });
 });
 
 module.exports = router;
