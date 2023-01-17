b879c6c4f4da9775247b6553efef3347a6103eb3
Nikidelfierro <landrieltristan1@gmail.com>
2023-01-17 16:41:13 -0300

    session v2

--- src/app.js
+++ src/app.js
@@ -1,38 +1,69 @@
 const path = require("path");
 require("dotenv").config();
-const express = require ("express");
-const app = express();
+const express = require ("express");   
 const bodyParser = require ("body-parser");
 const session = require ("express-session");
 const cookieParser = require ("cookie-parser");
-
+const app = express();
+const fs = require ("fs");
 app.use (express.static(path.join(__dirname, "../public")));
 app.use (bodyParser.urlencoded({ extended: true}));
+const bcrypt = ("bcrypt");
 
 
 app.set("view engine","ejs");
 
-const oneDay = 1000* 60* 60* 24;
-const halfDay = oneDay / 2;
 
-app.use(session({
-    saveUninitialized:true,
+
+app.use(
+    session({
+    saveUninitialized: true,
     secret: process.env.SESSION_SECRET,
-    cookie: {maxAge: oneDay},
+    cookie: {maxAge: 1000* 60 * 60 * 24},
     resave: false,
-}));
+}
+));
+
+
+app.get("/signup", (req, res)=> {
+        res.render("vistas/signup.ejs");
+});
+
+app.post("/signup", (req, res)=>{
+    const {user, pass} = req.body;
+
+    const file =     fs.writeFileSync(path.join(__dirname, "./user.json"));
+    const parsedFile = JSON.parse(file);
+
+    parsedFile = [...parsedFile, {
+        user,
+        pass: bcrypt.hash(pass, 10),
+
+    },]
+
+    fs.writeFileSync(path.join(__dirname, "./user.json"));
+
+
+    res.status(200);
+});
+
+
+app.get("/signin", (req, res)=>{
+    const {user, pass} = req.body;
+    res.status(200);
+});
+
 
 
 const homeRoutes = require ("./routes/home");
 const productRoutes = require ("./routes/products");
 const userRoutes = require ("./routes/users");
-const loginRoutes = require ("./routes/signin");
+
 
 app.use("/",userRoutes);
 app.use("/", homeRoutes);
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
-app.use("/signin",loginRoutes);
 
 
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,19 +1,7 @@
 const express = require("express");
 const { getUserForm, displayUserData } = require("../controllers/users");
-const app = require ("../app");
 
 const router = express.Router();
-
-app.get("/signin", (req,res) => {
-    res.render("../vistas/login.ejs");
-});
-
-app.post ("/signin", (req,res)=>{
-    const {user, password} = req.body;
-    console.log ("userName: ",user),
-    console.log ("pass: ",password ),
-    res.status(200);
-});
 
 router.get("/users", getUserForm);
 
